0e922103b090e100fcf1bfa52928ef2bacbfdad1
babotavo419 <one.faith419@gmail.com>
2023-08-03 07:26:57 -0400

    ok

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 require('dotenv').config()
@@ -7,35 +9,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 const PORT = process.env.PORT || 5000;
-
 let users = []; // this array will act as the in-memory database
+const saltRounds = 10;
 
-app.get('/api/users', (req, res) => {
-  res.json(users);
-});
+const secret = 'your-jwt-secret';
 
-app.post('/api/register', (req, res) => {
-  // here you should add some validation for username and password
-  const { username, password } = req.body;
-
-  // normally you would encrypt the password here
-  const user = { username, password };
-  users.push(user);
-
-  res.status(201).json(user);
-});
-
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // here you would normally check the password against the hashed one in the DB
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) {
-    res.json({ message: `Welcome ${user.username}!` });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
+// Middleware to Authenticate JWT
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token == null) return res.sendStatus(401);
+  
+    jwt.verify(token, secret, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
   }
+  
+  app.get('/api/users', authenticateToken, (req, res) => {
+    res.json(users);
+  });
+
+app.post('/api/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = { username, password: hashedPassword };
+    users.push(user);
+    res.status(201).json(user);
+});
+
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username: user.username }, secret);
+        res.json({ message: `Welcome ${user.username}!`, token });
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
 });
 
 app.listen(PORT, () => {
